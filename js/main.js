63d159d88368d46aa048011ea1cd9b957578825e
erynjones <eryn.jones16@hotmail.com>
2016-03-15 15:56:49 -0400

    trying to set it up

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var $html = $('html');
+
+$html.on('keydown', function () {
+  console.log(e.keyCode);
+  var div = document.createElement('div');
+  var rand = Math.floor(Math.random() * (max - min)) + min;
+});
