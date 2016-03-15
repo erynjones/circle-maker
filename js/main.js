b4cccb262cdf55e49edae2dc3ed54667e08c236d
erynjones <eryn.jones16@hotmail.com>
2016-03-15 16:23:30 -0400

    It works

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 var $html = $('html');
+var $body = $('body');
 
-$html.on('keydown', function () {
-  console.log(e.keyCode);
-  var div = document.createElement('div');
-  var rand = Math.floor(Math.random() * (max - min)) + min;
+$html.on('keydown', function (e) {
+  var div = $('<div>');
+  $body.append(div);
+  div.addClass('ball');
+  div.css('top', Math.random() * 1000);
+  div.css('left', Math.random() * 1000);
 });
